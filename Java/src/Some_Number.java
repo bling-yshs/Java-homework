395124990583681c2a8344a510af666265f6acf9
一闪hs <41812674+bling-yshs@users.noreply.github.com>
2022-09-08 14:01:28 +0800

    fs

---
+++ Java/src/Some_Number.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Some_Number {
+    public int getsum(int n) {
+        int i = 0;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int sum = 0;
+        int flag = 0;
+        while (i <= n) {
+            b = i;
+            c = (int) Math.log10(b);
+            for (int j = 0; j <= c; j++) {
+                a = b % 10;
+                if (a == 2 || a == 0 || a == 1 || a == 9) {
+                    b = b / 10;
+                    flag = 1;
+                } else {
+                    flag = 0;
+                }
+            }
+            if (flag == 1) {
+                sum += i;
+            }
+
+            i++;
+        }
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum = app.getsum(n);
+        System.out.println(sum);
+        in.close();
+    }
+
+}
