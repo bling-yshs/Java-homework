7c69b8f21cc87d2a31ddd3f6dea059a235e4a083
bling_yshs <mcyishan@outlook.com>
2022-09-10 10:41:55 +0800

    ignore

---
+++ Java/src/SortTest.java
@@ -0,0 +1,13 @@
+public class SortTest {
+    public static void main(String[] args) {
+        System.out.println("--------------------------");
+        System.out.println("Hello World!");
+        System.out.println("以下为输出↓");
+        System.out.println("");
+        
+        
+        
+        System.out.println("");
+        System.out.println("--------------------------");
+    }
+}
